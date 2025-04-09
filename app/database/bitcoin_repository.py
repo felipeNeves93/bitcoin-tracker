597f18711a002702f05d04a7980d542665768e76
felipe-neves <felipe.neves@connectist.com>
2025-04-09 12:53:00 -0300

    - Fixed session management repository

--- app/database/bitcoin_repository.py
+++ app/database/bitcoin_repository.py
@@ -10,59 +10,77 @@ from app.database.model.bitcoin_summary import BitcoinSummary
 
 class BitcoinRepository:
 
-    def __init__(self, db: Session):
-        self.db = db
+    def __init__(self, session: Session):
+        self.session = session
 
     def insert_price(self, price: float, timestamp=None):
         bitcoin_price = BitcoinPrice(price=price, timestamp=timestamp)
 
-        self.db.add(bitcoin_price)
-        self.db.commit()
-        self.db.refresh(bitcoin_price)
+        self.session.add(bitcoin_price)
+        self.session.commit()
+        self.session.refresh(bitcoin_price)
 
     def get_latest_price(self) -> Optional[BitcoinPrice]:
-        return self.db.query(BitcoinPrice).order_by(BitcoinPrice.timestamp.desc()).first()
+        try:
+            return self.session.query(BitcoinPrice).order_by(BitcoinPrice.timestamp.desc()).first()
+        finally:
+            self.session.close()
 
     def update_summary(self, price: float, day: date):
-        existing_bitcoin_summary = self.db.query(BitcoinSummary).filter(
-            BitcoinSummary.day == day).first()
+        try:
+            existing_bitcoin_summary = self.session.query(BitcoinSummary).filter(
+                BitcoinSummary.day == day).first()
 
-        if existing_bitcoin_summary is None:
-            print(f"Adding new summary for the day {day}")
-            summary_to_add = BitcoinSummary(min_price=price, max_price=price, day=day)
-            self.db.add(summary_to_add)
-            self.db.commit()
-            self.db.refresh(summary_to_add)
+            if existing_bitcoin_summary is None:
+                print(f"Adding new summary for the day {day}")
+                summary_to_add = BitcoinSummary(min_price=price, max_price=price, day=day)
+                self.session.add(summary_to_add)
+                self.session.commit()
+                self.session.refresh(summary_to_add)
 
-            return None
+                return None
 
-        print(f"Existing summary {existing_bitcoin_summary.id} for the day {day}. Updating prices if needed")
+            print(f"Existing summary {existing_bitcoin_summary.id} for the day {day}. Updating prices if needed")
 
-        if existing_bitcoin_summary.max_price < price:
-            existing_bitcoin_summary.max_price = price
+            if existing_bitcoin_summary.max_price < price:
+                existing_bitcoin_summary.max_price = price
 
-        if existing_bitcoin_summary.min_price > price:
-            existing_bitcoin_summary.min_price = price
+            if existing_bitcoin_summary.min_price > price:
+                existing_bitcoin_summary.min_price = price
 
-        self.db.add(existing_bitcoin_summary)
-        self.db.commit()
-        self.db.refresh(existing_bitcoin_summary)
+            self.session.add(existing_bitcoin_summary)
+            self.session.commit()
+            self.session.refresh(existing_bitcoin_summary)
+        finally:
+            self.session.close()
 
     def get_summary_by_day(self, day: date) -> Optional[BitcoinSummary]:
-        return self.db.query(BitcoinSummary).filter(BitcoinSummary.day == day).first()
+        try:
+            return self.session.query(BitcoinSummary).filter(BitcoinSummary.day == day).first()
+        finally:
+            self.session.close()
 
     def get_all_summaries(self) -> list[Type[BitcoinSummary]]:
-        return self.db.query(BitcoinSummary).all()
+        try:
+            return self.session.query(BitcoinSummary).all()
+        finally:
+            self.session.close()
 
     def delete_prices_older_than_90_days(self):
-        date_to_delete = datetime.now() - timedelta(days=90)
+        try:
+            date_to_delete = datetime.now() - timedelta(days=90)
 
-        print(f"Date to cut: {date_to_delete}")
+            print(f"Date to cut: {date_to_delete}")
 
-        result = self.db.query(BitcoinPrice).filter(BitcoinPrice.timestamp < date_to_delete).delete()
-        self.db.commit()
+            result = self.session.query(BitcoinPrice).filter(BitcoinPrice.timestamp < date_to_delete).delete()
+            self.session.commit()
 
-        return result
+            return result
+        finally:
+            self.session.close()
 
     def get_all_prices(self) -> list[type[BitcoinPrice]]:
-        return self.db.query(BitcoinPrice).all()
+        try:
+            return self.session.query(BitcoinPrice).all()
+        finally:
+            self.session.close()
